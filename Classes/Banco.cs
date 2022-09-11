597f29559e8d66e2de943cb61a4de9f91d555621
Lucas Celestino <lucascap899@gmail.com>
2022-09-11 14:45:07 -0300

    finalizando classe conta e banco

--- Classes/Banco.cs
+++ Classes/Banco.cs
@@ -7,10 +7,10 @@ namespace digibank.Classes
 {
     public class Banco
     {
-        public Banco(string nomeBanco, string codigoBanco)
+        public Banco()
         {
-            this.nomeBanco = nomeBanco;
-            this.codigoBanco = codigoBanco;
+            this.nomeBanco = "DigiBank";
+            this.codigoBanco = "027";
         }
 
         public string nomeBanco {get; private set;}
--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -10,23 +10,37 @@ namespace digibank.Classes
     {
         public Conta()
         {
-            
+            this.numeroConta = "0001";
+            this.numeroAgencia = "";
+            Conta.numeroContaSequencial++;
         }
+
+        public double saldo {get; protected set;}
+        public string numeroAgencia {get; private set;}
+        public string numeroConta {get; private set;}
+
+        public static int numeroContaSequencial {get; private set;}
         public void deposita(double valor)
         {
-
+            this.saldo += valor;
         }
         public bool saca(double valor)
         {
+            if(valor > this.consultaSaldo())
+            {
+                return false;
+            }
 
+            this.saldo -= valor;
+            return true;
         }
         public double consultaSaldo()
         {
-
+            return this.saldo;
         }
         public string getCodigoBanco()
         {
-
+            return this.codigoBanco;
         }
         public string getNumeroAgencia()
         {
@@ -34,7 +48,7 @@ namespace digibank.Classes
         }
         public string getNumeroConta()
         {
-
+            return this.numeroConta;
         }
     }
 }
