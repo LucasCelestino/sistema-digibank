31d2b75a251a5c6527471a9a966e8395220e4d56
Lucas Celestino <lucascap899@gmail.com>
2022-09-11 14:01:03 -0300

    adicionando telas do sistema

---
+++ Classes/Layout.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digibank.Classes
+{
+    public class Layout
+    {
+        private static int opcao = 0;
+        public static void telaPrincipal()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Clear();
+
+            Console.WriteLine("                                                        ");
+            Console.WriteLine("                 Digite a opção desejada:               ");
+            Console.WriteLine("                 ============================           ");
+            Console.WriteLine("                 1 - Criar Conta                        ");
+            Console.WriteLine("                 ============================           ");
+            Console.WriteLine("                 2 - Entrar com CPF e Senha             ");
+            Console.WriteLine("                 ============================           ");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    telaCriarConta();
+                    break;
+                case 2:
+                    telaLogin();
+                    break;
+                default:
+                    Console.WriteLine("Opção não disponível");
+                    break;
+            }
+            
+        }
+
+        public static void telaCriarConta()
+        {
+            Console.Clear();
+
+            Console.WriteLine("                                                        ");
+            Console.WriteLine("                 Digite o nome:                     ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("                 Digite o CPF:                      ");
+            string cpf = Console.ReadLine();
+            Console.WriteLine("                 Digite a senha:                    ");
+            string senha = Console.ReadLine();
+            Console.WriteLine("                 ============================           ");
+
+            Console.WriteLine(nome);
+            Console.WriteLine(cpf);
+            Console.WriteLine(senha);
+        }
+
+        public static void telaLogin()
+        {
+            Console.Clear();
+
+            Console.WriteLine("                                                        ");
+            Console.WriteLine("                 Digite o seu CPF:                      ");
+            string cpf = Console.ReadLine();
+            Console.WriteLine("                 Digite a sua senha:                    ");
+            string senha = Console.ReadLine();
+            Console.WriteLine("                 ============================           ");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using digibank.Classes;
+
+namespace digibank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Layout.telaPrincipal();
+        }
+    }
+}
